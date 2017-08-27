015e3b33e55ff6c0767cc8aecece470c8f4d3fcb
Matt Crowder <matt.crowder@walmart.com>
2017-08-27 13:32:57 -0500

    try some more

--- src/express-rest-mongo.js
+++ src/express-rest-mongo.js
@@ -5,38 +5,40 @@ import "babel-polyfill";
 
 import factory from "./factory";
 import router from "./routes/routes";
+/*eslint no-invalid-this: "off"*/
 const ExpressRestMongo = config => {
     if (!config.port) {
         throw new Error("port required");
     }
     this.port = config.port;
-    //TODO add custom routes
+    // TODO add custom routes
     // this.customRoutes = config.customRoutes || {};
-};
 
-ExpressRestMongo.protoType.run = () => {
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended: true}));
-    app.use(router);
+    this.run = () => {
+        const app = express();
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(router);
 
 
-    app.post("*", async (req, res) => {
-        try {
-            const arr = req.originalUrl.split("/").filter(e => e !== "");
-            const result = await factory(arr[0], arr[1], req.body);
-            res.send(result);
-        } catch (error) {
-            console.log("error in root ", error);
-            res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error.message);
-        }
-    });
-    const {port} = this;
-    app.listen(port, () => {
-        /*eslint no-console: "off"*/
-        /*global console: true*/
-        console.log(`Server started on port ${port}`);
-    });
+        app.post("*", async (req, res) => {
+            try {
+                const arr = req.originalUrl.split("/").filter(e => e !== "");
+                const result = await factory(arr[0], arr[1], req.body);
+                res.send(result);
+            } catch (error) {
+                console.log("error in root ", error);
+                res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(error.message);
+            }
+        });
+        const {port} = this;
+        app.listen(port, () => {
+            /*eslint no-console: "off"*/
+            /*global console: true*/
+            console.log(`Server started on port ${port}`);
+        });
+    };
+
 };
 
 export default ExpressRestMongo;
