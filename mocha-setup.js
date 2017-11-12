c817db9e13f60d65d690ec84761659375a235677
Matt Crowder <mcrowder65@gmail.com>
2017-11-11 22:27:44 -0600

    test jwt

--- mocha-setup.js
+++ mocha-setup.js
@@ -1,5 +1,9 @@
+require("babel-core/register");
+require("babel-polyfill");
 const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 
+chai.use(chaiAsPromised);
 chai.config.includeStack = true;
 
 global.expect = chai.expect;
--- test/utilities/jwt.spec.js
+++ test/utilities/jwt.spec.js
@@ -1,5 +1,15 @@
+import jwt from "../../src/utilities/jwt";
+
 describe("test/utilities/jwt.spec.js", () => {
-    it("generateToken", () => {
-        expect(1).eql(1);
+    it("generateToken then verify it", async () => {
+        const username = "hello";
+        const _id = "world";
+        const token = jwt.generateToken("hello", "world");
+        const decodedToken = await jwt.verifyToken(token);
+        expect(decodedToken.username).equal(username);
+        expect(decodedToken._id).equal(_id);
+    });
+    it("try to verify a bad token", async () => {
+        expect(Promise.resolve(jwt.verifyToken("asdf"))).to.be.rejected;
     });
 });
