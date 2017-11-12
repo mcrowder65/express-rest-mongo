749fd0854bd66f164566be1c6286887fe912da64
Matt Crowder <mcrowder65@gmail.com>
2017-11-12 08:47:23 -0600

    edge cases

--- test/utilities/jwt.spec.js
+++ test/utilities/jwt.spec.js
@@ -4,7 +4,47 @@ describe("test/utilities/jwt.spec.js", () => {
     it("generateToken then verify it", async () => {
         const username = "hello";
         const _id = "world";
-        const token = jwt.generateToken("hello", "world");
+        const token = jwt.generateToken(username, _id);
+        const decodedToken = await jwt.verifyToken(token);
+        expect(decodedToken.username).equal(username);
+        expect(decodedToken._id).equal(_id);
+    });
+    it("generateToken with null params then verify it", async () => {
+        const username = null;
+        const _id = null;
+        const token = jwt.generateToken(username, _id);
+        const decodedToken = await jwt.verifyToken(token);
+        expect(decodedToken.username).equal(username);
+        expect(decodedToken._id).equal(_id);
+    });
+    it("generateToken with null param then verify it", async () => {
+        const username = null;
+        const _id = "hi!";
+        const token = jwt.generateToken(username, _id);
+        const decodedToken = await jwt.verifyToken(token);
+        expect(decodedToken.username).equal(username);
+        expect(decodedToken._id).equal(_id);
+    });
+    it("generateToken with undefined params then verify it", async () => {
+        const username = undefined;
+        const _id = undefined;
+        const token = jwt.generateToken(username, _id);
+        const decodedToken = await jwt.verifyToken(token);
+        expect(decodedToken.username).equal(username);
+        expect(decodedToken._id).equal(_id);
+    });
+    it("generateToken with undefined param then verify it", async () => {
+        const username = undefined;
+        const _id = "hi!";
+        const token = jwt.generateToken(username, _id);
+        const decodedToken = await jwt.verifyToken(token);
+        expect(decodedToken.username).equal(username);
+        expect(decodedToken._id).equal(_id);
+    });
+    it("generateToken with number params then verify it", async () => {
+        const username = 1234;
+        const _id = 444;
+        const token = jwt.generateToken(username, _id);
         const decodedToken = await jwt.verifyToken(token);
         expect(decodedToken.username).equal(username);
         expect(decodedToken._id).equal(_id);
