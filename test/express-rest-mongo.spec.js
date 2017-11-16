1ecf844c5a3a5dd845121f0c9a7ad925c08a0b51
Matt Crowder <mcrowder65@gmail.com>
2017-11-15 21:11:40 -0600

    testsss

--- test/express-rest-mongo.spec.js
+++ test/express-rest-mongo.spec.js
@@ -1,13 +1,33 @@
+import uniqueString from "unique-string";
+
 import ExpressRestMongo from "../src/express-rest-mongo";
 
+//this file is pretty much temporary and express-rest-mongo will be filled more thoroughly
+//when other tests are written using it explicitly!
 describe("test/express-rest-mongo.spec.js", () => {
-    test("run!", () => {
+    test("run with user defined arguments", () => {
         const app = new ExpressRestMongo({
             port: 3000,
-            db: "tempDb",
+            db: uniqueString(),
             mongoPort: 27017
         });
         app.run();
         app.stop();
+        //no errors should happen
+    });
+    test("run with default arguments", () => {
+        const app = new ExpressRestMongo({db: uniqueString()});
+        app.run();
+        app.stop();
+        //no errors should happen
+    });
+    test("no db should throw", () => {
+        try {
+            //eslint-disable-next-line
+            const app = new ExpressRestMongo({});
+            expect(true).toEqual(false);
+        } catch (e) {
+            expect(e.message).toEqual("db required");
+        }
     });
 });
